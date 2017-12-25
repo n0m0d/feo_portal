d941f813d17183ad07a946a4bfe4d6f6102dba47
n0m0d <n0m0d95@gmail.com>
2017-12-25 20:20:22 +0300

    1

--- app/js/common.js
+++ app/js/common.js
@@ -1,5 +1,20 @@
 $(function() {
 
-	// Custom JS
+	$("#city-filter").chosen({
+		disable_search: false,
+		//width: "100%"
+	});
+
+	$(".toggle-menu").on("click", function() {
+		$(this).toggleClass("on");
+		$(".mobile-block-menu").slideToggle();
+		return false;
+	});
+
+	$(".has-drop-mobile").on("click", function() {
+		$(this).find('ul').slideToggle();
+		$(this).toggleClass("open");
+		return false;
+	});
 
 });
