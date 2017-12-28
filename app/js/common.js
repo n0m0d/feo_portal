b074c6660dc81a585be6dbcaf3a05117e768c32f
n0m0d <n0m0d95@gmail.com>
2017-12-28 18:55:03 +0300

    index END

--- app/js/common.js
+++ app/js/common.js
@@ -62,6 +62,21 @@ $(function() {
 		return rows;
 	}slider_3_rows();
 
+	function slider_4_rows() {
+
+		var viewportWidth = $(window).width();
+
+		if(viewportWidth > 992) {
+			var rows = 2;
+		} else if(viewportWidth < 480) {
+			var rows = 2;
+		} else {
+			var rows = 1;
+		}
+
+		return rows;
+	}slider_4_rows();
+
 	$(".slider-style-1").slick({
 		slidesToShow: 1,
 		slidesToScroll: 1,
@@ -148,6 +163,39 @@ $(function() {
 				settings: {
 					arrows: false,
 					slidesToShow: 2,
+					slidesToScroll: 1,
+				}
+			},
+			{
+				breakpoint: 480,
+				settings: {
+					arrows: false,
+					slidesToShow: 1,
+					slidesToScroll: 1
+				}
+			}
+		]
+	});
+
+	$(".slider-style-4").slick({
+		slidesToShow: 2,
+		slidesToScroll: 1,
+		rows: slider_4_rows(),
+		dots: false,
+		arrows: false,
+		centerMode: false,
+		infinite: true,
+		speed: 500,
+		fade: false,
+		cssEase: 'linear',
+		autoplay: false,
+ 		autoplaySpeed: 2000,
+ 		responsive: [
+			{
+				breakpoint: 992,
+				settings: {
+					arrows: false,
+					slidesToShow: 3,
 					slidesToScroll: 1,
 				}
 			},
